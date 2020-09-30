6e07f432dd664504415ee0c58fd88a0dc6152954
东软小师兄 <392358144@qq.com>
2020-09-30 16:30:41 +0800

    Update routes.php
    推出登录取消Auth验证

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -16,7 +16,7 @@ Route::group(
         $p    = config('cas.router.name_prefix');
         Route::get('login', 'SecurityController@showLogin')->name($p.'login.get');
         Route::post('login', 'SecurityController@login')->name($p.'login.post');
-        Route::get('logout', 'SecurityController@logout')->name($p.'logout')->middleware($auth);
+        Route::get('logout', 'SecurityController@logout')->name($p.'logout');
         Route::any('validate', 'ValidateController@v1ValidateAction')->name($p.'v1.validate');
         Route::any('serviceValidate', 'ValidateController@v2ServiceValidateAction')->name($p.'v2.validate.service');
         Route::any('proxyValidate', 'ValidateController@v2ProxyValidateAction')->name($p.'v2.validate.proxy');
